dbf5ca9e84ae284df53cdfc0904a0c02157bc90c
Rafael Boszko <rafael.boszko-lucena@wrenkitchens.com>
2022-04-18 18:27:20 +0200

    0002 feat: setup arguments architecture (filtering in two lists)

--- gitbro/abc/Arguments.py
+++ gitbro/abc/Arguments.py
@@ -1,8 +1,26 @@
+import re as regex
 import sys as system
 
 class Arguments:
     def __init__(self) -> None:
-        print(system.argv)
+        count = 0
+
+        self.options = []
+        self.values = []
+        for i, command_argument in enumerate(system.argv):
+            if (count == 0):
+                count += 1
+                continue
+
+            if (self.__is_option(command_argument)):
+                self.options.append(command_argument)
+            else:
+                self.values.append(command_argument)
+
+        # print(self.values)
+
+    def __is_option(self, command_argument: str):
+        return regex.search('-\w+', command_argument)
 
 if __name__ == "__main__":
     a = Arguments()
