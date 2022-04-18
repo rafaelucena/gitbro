94dd53f38bc250836111588a7b8bd598bd7a9de0
Rafael Boszko <rafael.boszko-lucena@wrenkitchens.com>
2022-04-18 11:59:45 +0200

    0001 feat: setup gist command (configuration changes)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gitbro',
-    version='0.1',
+    version='0.2.0',
     description='A bunch of handy commands to do things a bit faster using git command line (imho)',
     url='http://github.com/rafaelucena/gitbro',
     author='Rafael Boszko',
@@ -11,7 +11,8 @@ setup(
     packages=find_packages(),
     entry_points=dict(
         console_scripts=[
-            'gitbro=gitbro.mock.main:run'
+            'gitbro=gitbro.mock.main:run',
+            'gist=gitbro.gist.main:run',
         ]
     )
 )
