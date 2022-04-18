3420fc338b27c064641b3b8f20cfaaa09f4e5967
Rafael Boszko <rafael.boszko-lucena@wrenkitchens.com>
2022-04-18 21:19:56 +0200

    0003 feat: setup gibr command (command basic layout)

--- gitbro/gibr/Command.py
+++ gitbro/gibr/Command.py
@@ -1,9 +1,21 @@
-import os
+from gitbro.abc.Arguments import Arguments
 
 class Command:
+    options: list = []
+    values: list = []
+
+    def __init__(self) -> None:
+        arguments = Arguments()
+        self.options = arguments.get_options()
+        self.values = arguments.get_values()
+
+        # print(self.options)
+
     def run(self):
-        print('git branch')
-        os.system('git branch')
+        if len(self.options) > 0:
+            print('evaluate options')
+        elif len(self.values) > 0:
+            print('get to branch')
 
 if __name__ == "__main__":
     command = Command()
