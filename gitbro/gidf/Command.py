b833d6e05e7923717eeb5e2253db8d84aa431421
Rafael Boszko <rafael.boszko-lucena@wrenkitchens.com>
2022-04-20 01:13:17 +0200

    0005 feat: setup gidf command (sketching command for gidf)

--- gitbro/gidf/Command.py
+++ gitbro/gidf/Command.py
@@ -1,9 +1,30 @@
-import os
+from gitbro.abc.Arguments import Arguments
 
 class Command:
+    options: list = []
+    values: list = []
+
+    def __init__(self) -> None:
+        arguments = Arguments()
+        self.options = arguments.get_options()
+        self.values = arguments.get_values()
+
     def run(self):
-        print('git status')
-        os.system('git status')
+        if len(self.options) > 0:
+            self.__run_options()
+        elif len(self.values) > 0:
+            self.__run_values()
+        else:
+            self.__run_default()
+
+    def __run_options(self):
+        print('this option is not mapped (yet)')
+
+    def __run_values(self):
+        print('find a file and show its diff')
+
+    def __run_default(self):
+        print('run the git diff --stat')
 
 if __name__ == "__main__":
     command = Command()
