93601e7435ef215f2424d396c3533ee6d173a8f4
Rafael Boszko <rafael.boszko-lucena@wrenkitchens.com>
2022-04-21 02:15:09 +0200

    0005 feat: setup gidf command (gidf main diff path for files, with wildcard included)

---
+++ gitbro/gidf/BashGitDiffFile.py
@@ -0,0 +1,36 @@
+import os
+import re as regex
+
+class BashGitDiffFile:
+    line: str = '{base} {action} {target}' # @todo - ":extras:"
+    base: str = 'git'
+    action: str = 'diff'
+    target: str = ''
+
+    def __init__(self, options: list = [], values: list = []) -> None:
+        command = self.__map_command(options, values)
+
+        # @todo - colorful print - print('{0} {1} {2}'.format('\033[32mgit', self.action, 'option'))
+        print(command)
+        os.system(command)
+
+    def __map_command(self, options: list = [], values: list = []):
+        if len(values) > 0:
+            self.target = self.__prepare_diff_value(values[0])
+
+        self.line = self.line.format(base=self.base, action=self.action, target=self.target)
+
+        return self.line
+
+    def __prepare_diff_value(self, value):
+        target_value = ''
+        if regex.search('\.\w?', value):
+            target_value = '*{file_name}'.format(file_name=value)
+        else:
+            target_value = '*{file_name}*'.format(file_name=value)
+
+        return target_value
+
+    @staticmethod
+    def go(options: list = [], values: list = []):
+        BashGitDiffFile(options, values)
