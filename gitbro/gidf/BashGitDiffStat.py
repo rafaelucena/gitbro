5755ffdb572a920ad62788161b9501b7d10816b5
Rafael Boszko <rafael.boszko@gmail.com>
2022-04-21 02:06:43 +0200

    0005 feat: setup gidf command (value wildcard improved)

--- gitbro/gidf/BashGitDiffStat.py
+++ gitbro/gidf/BashGitDiffStat.py
@@ -1,4 +1,5 @@
 import os
+import re as regex
 
 class BashGitDiffStat:
     line: str = '{base} {action} {target}' # @todo - ":extras:"
@@ -15,11 +16,20 @@ class BashGitDiffStat:
 
     def __map_command(self, options: list = [], values: list = []):
         if len(values) > 0:
-            self.target = values[0]
+            self.target = self.__prepare_diff_value(values[0])
 
         self.line = self.line.format(base=self.base, action=self.action, target=self.target)
 
         return self.line
+
+    def __prepare_diff_value(self, value):
+        target_value = ''
+        if regex.search('\.\w?', value):
+            target_value = '*{file_name}'.format(file_name=value)
+        else:
+            target_value = '*{file_name}*'.format(file_name=value)
+
+        return target_value
 
     @staticmethod
     def go(options: list = [], values: list = []):
