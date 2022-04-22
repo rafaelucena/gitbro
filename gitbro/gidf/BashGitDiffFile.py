829c0986fc3c5af42e7874382a4bbd0f08428262
Rafael Boszko <rafael.boszko-lucena@wrenkitchens.com>
2022-04-22 18:48:47 +0200

    0005 feat: setup gidf command (files with case sensitive ignored working)

--- gitbro/gidf/BashGitDiffFile.py
+++ gitbro/gidf/BashGitDiffFile.py
@@ -1,3 +1,4 @@
+import subprocess
 import os
 import re as regex
 
@@ -23,6 +24,8 @@ class BashGitDiffFile:
         return self.line
 
     def __prepare_diff_value(self, value):
+        value = self.__prepare_case_insensitive_argument_search(value)
+
         target_value = ''
         if regex.search('\.\w?', value):
             target_value = '*{file_name}'.format(file_name=value)
@@ -30,6 +33,34 @@ class BashGitDiffFile:
             target_value = '*{file_name}*'.format(file_name=value)
 
         return target_value
+
+    def __prepare_case_insensitive_argument_search(self, value):
+        parsed_lines = {}
+        mapped_needles = {}
+        is_case_insensitive_argument_found = False
+
+        changed_files = subprocess.getoutput('git diff --name-only')
+        for changed_line in changed_files.splitlines():
+            parsed_lines[changed_line] = changed_line.lower()
+
+            tracked_argument_case_sensitive = changed_line.rfind(value)
+            if tracked_argument_case_sensitive != -1:
+                return value
+
+            tracked_argument_case_insensitive = parsed_lines[changed_line].rfind(value)
+            if tracked_argument_case_insensitive != -1:
+                is_case_insensitive_argument_found = True
+
+                needle_to_map = changed_line[tracked_argument_case_insensitive:tracked_argument_case_insensitive+len(value)]
+                if needle_to_map in mapped_needles:
+                    mapped_needles[needle_to_map] += 1
+                else:
+                    mapped_needles[needle_to_map] = 1
+
+        if is_case_insensitive_argument_found == True:
+            return next(iter(mapped_needles))
+
+        return value
 
     @staticmethod
     def go(options: list = [], values: list = []):
