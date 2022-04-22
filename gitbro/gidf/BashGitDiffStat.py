b535dba198889f84d1324176f04eab1a49c06e14
Rafael Boszko <rafael.boszko@gmail.com>
2022-04-22 21:39:46 +0200

    0005 feat: setup gidf command (fixing -s variant for ignoring case)

--- gitbro/gidf/BashGitDiffStat.py
+++ gitbro/gidf/BashGitDiffStat.py
@@ -1,10 +1,12 @@
+import subprocess
 import os
 import re as regex
+
 
 class BashGitDiffStat:
     line: str = '{base} {action} {target}' # @todo - ":extras:"
     base: str = 'git'
-    action: str = 'diff --stat'
+    action: str = 'diff {head} --stat'
     target: str = ''
 
     def __init__(self, options: list = [], values: list = []) -> None:
@@ -17,12 +19,17 @@ class BashGitDiffStat:
     def __map_command(self, options: list = [], values: list = []):
         if len(values) > 0:
             self.target = self.__prepare_diff_value(values[0])
+            self.action = self.action.format(head='')
+        else:
+            self.action = self.action.format(head='HEAD')
 
         self.line = self.line.format(base=self.base, action=self.action, target=self.target)
 
         return self.line
 
     def __prepare_diff_value(self, value):
+        value = self.__prepare_diff_value_search(value)
+
         target_value = ''
         if regex.search('\.\w?', value):
             target_value = '*{file_name}'.format(file_name=value)
@@ -30,6 +37,18 @@ class BashGitDiffStat:
             target_value = '*{file_name}*'.format(file_name=value)
 
         return target_value
+
+    def __prepare_diff_value_search(self, value):
+        parsed_lines = {}
+        changed_files = subprocess.getoutput('git diff --name-only')
+        for changed_line in changed_files.splitlines():
+            parsed_lines[changed_line] = changed_line.lower()
+
+            tracked_argument = parsed_lines[changed_line].rfind(value)
+            if tracked_argument != -1:
+                return (changed_line[tracked_argument:tracked_argument+len(value)])
+
+        return value
 
     @staticmethod
     def go(options: list = [], values: list = []):
