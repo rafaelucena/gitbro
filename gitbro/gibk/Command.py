e0ae4b78a75065b25d5d75aedf08a4a3b36917ff
Rafael Boszko <rafael.boszko-lucena@wrenkitchens.com>
2022-04-25 20:55:11 +0200

    0007 feat: setup gibk command (-s variant added)

--- gitbro/gibk/Command.py
+++ gitbro/gibk/Command.py
@@ -1,9 +1,34 @@
-import os
+from gitbro.abc.Arguments import Arguments
+from gitbro.gibk.BashGitStashPush import BashGitStashPush
 
 class Command:
+    options: list = []
+    values: list = []
+
+    def __init__(self) -> None:
+        arguments = Arguments()
+        self.options = arguments.get_options()
+        self.values = arguments.get_values()
+
     def run(self):
-        print('git stash list')
-        os.system('git stash list')
+        if len(self.options) > 0:
+            self.__run_options()
+        elif len(self.values) > 0:
+            self.__run_values()
+        else:
+            self.__run_default()
+
+    def __run_options(self):
+        if self.options[0] == '-s':
+            BashGitStashPush.go(self.options, self.values)
+        else:
+            print('this option is not mapped (yet)')
+
+    def __run_values(self):
+        print('values is not mapped (yet)')
+
+    def __run_default(self):
+        print('default is not mapped (yet)')
 
 if __name__ == "__main__":
     command = Command()
