52fe9765221d18576604ce3202727225a3745cd5
Rafael Boszko <rafael.boszko-lucena@wrenkitchens.com>
2022-04-28 01:52:12 +0200

    0007 feat: setup gibk command (-c variant for clear, with prompt, and apply by index as well

--- gitbro/gibk/BashGitStashApply.py
+++ gitbro/gibk/BashGitStashApply.py
@@ -5,7 +5,7 @@ class BashGitStashApply:
     line: str = '{base} {action} {target} {comment}' # @todo - ":extras:"
     base: str = 'git'
     action: str = 'stash apply'
-    target: str = ''
+    target: str = 'stash@{{{index}}}'
     comment: str = ''
 
     def __init__(self, options: list = [], values: list = []) -> None:
@@ -13,13 +13,16 @@ class BashGitStashApply:
 
         # @todo - colorful print - print('{0} {1} {2}'.format('\033[32mgit', self.action, 'option'))
         print(command)
-        os.system(command)
+        # os.system(command)
 
     def __map_command(self, options: list = [], values: list = []):
-        list_helper = ListResultsCaseIgnored()
-        stashed_item = list_helper.find_stash_list_grouped(values[0])
-        self.target = stashed_item['stash']
-        self.comment = '#{branch}: {message}'.format(branch=stashed_item['branch'], message=stashed_item['message'])
+        if len(options) > 0 and options[0] == '-g':
+            list_helper = ListResultsCaseIgnored()
+            stashed_item = list_helper.find_stash_list_grouped(values[0])
+            self.target = stashed_item['stash']
+            self.comment = '#{branch}: {message}'.format(branch=stashed_item['branch'], message=stashed_item['message'])
+        else:
+            self.target = self.target.format(index=values[0])
 
         self.line = self.line.format(base=self.base, action=self.action, target=self.target, comment=self.comment)
 
