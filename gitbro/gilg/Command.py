0acd994cfde5f7c9672705407c31ef2b2d976686
Rafael Boszko <rafael.boszko@gmail.com>
2022-04-30 22:00:03 +0200

    0010 feat: setup gilg command (addind log and -p variant for pretty formatting)

--- gitbro/gilg/Command.py
+++ gitbro/gilg/Command.py
@@ -1,9 +1,38 @@
-import os
+import re as regex
+
+from gitbro.abc.Arguments import Arguments
+from gitbro.gilg.BashGitLogRange import BashGitLogRange
 
 class Command:
+    options: list = []
+    values: list = []
+
+    def __init__(self) -> None:
+        arguments = Arguments()
+        self.options = arguments.get_options()
+        self.values = arguments.get_values()
+
     def run(self):
-        print('git log')
-        os.system('git log')
+        if len(self.options) > 0:
+            self.__run_options()
+        elif len(self.values) > 0:
+            self.__run_values()
+        else:
+            self.__run_default()
+
+    def __run_options(self):
+        if regex.search('^-(\d+)', self.options[0]): #logs
+            BashGitLogRange.go(self.options, self.values)
+        elif self.options[0] == '-p': #pretty
+            BashGitLogRange.go(self.options, self.values)
+        else:
+            print('this option is not mapped (yet)')
+
+    def __run_values(self):
+        BashGitLogRange.go(self.options, self.values)
+
+    def __run_default(self):
+        print('default is not mapped (yet)')
 
 if __name__ == "__main__":
     command = Command()
