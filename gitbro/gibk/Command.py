9af85fc3c5d749f652191424661f68a459112f32
Rafael Boszko <rafael.boszko-lucena@wrenkitchens.com>
2022-04-30 20:19:38 +0200

    0007 feat: setup gibk command (new variants for -v|view and -s|stat)

--- gitbro/gibk/Command.py
+++ gitbro/gibk/Command.py
@@ -5,6 +5,7 @@ from gitbro.gibk.BashGitStashDrop import BashGitStashDrop
 from gitbro.gibk.BashGitStashList import BashGitStashList
 from gitbro.gibk.BashGitStashPop import BashGitStashPop
 from gitbro.gibk.BashGitStashPush import BashGitStashPush
+from gitbro.gibk.BashGitStashView import BashGitStashView
 
 class Command:
     options: list = []
@@ -24,7 +25,7 @@ class Command:
             self.__run_default()
 
     def __run_options(self):
-        if self.options[0] == '-s': #save|push
+        if self.options[0] == '-n': #new|push
             BashGitStashPush.go(self.options, self.values)
         elif self.options[0] == '-c': #clear
             BashGitStashBoom.go(self.options, self.values)
@@ -38,6 +39,10 @@ class Command:
             BashGitStashPop.go(self.options, self.values)
         elif self.options[0] == '-g': #grep
             BashGitStashApply.go(self.options, self.values)
+        elif self.options[0] == '-s': #stat
+            BashGitStashView.go(self.options, self.values)
+        elif self.options[0] == '-v': #view
+            BashGitStashView.go(self.options, self.values)
         else:
             print('this option is not mapped (yet)')
 
