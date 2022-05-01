c20d0ef6c2e0dfcf302a4fbdb82b35f6c1a62b52
Rafael Boszko <rafael.boszko@gmail.com>
2022-05-01 22:28:31 +0200

    0011 feat: setup gime command (command updated)

--- gitbro/gime/Command.py
+++ gitbro/gime/Command.py
@@ -1,9 +1,30 @@
-import os
+from gitbro.abc.Arguments import Arguments
 
 class Command:
+    options: list = []
+    values: list = []
+
+    def __init__(self) -> None:
+        arguments = Arguments()
+        self.options = arguments.get_options()
+        self.values = arguments.get_values()
+
     def run(self):
-        print('git merge')
-        os.system('git merge')
+        if len(self.options) > 0:
+            self.__run_options()
+        elif len(self.values) > 0:
+            self.__run_values()
+        else:
+            self.__run_default()
+
+    def __run_options(self):
+        print('this option is not mapped (yet)')
+
+    def __run_values(self):
+        print('this value is not mapped (yet)')
+
+    def __run_default(self):
+        print('default is not mapped (yet)')
 
 if __name__ == "__main__":
     command = Command()
