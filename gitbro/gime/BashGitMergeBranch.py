256669609da31b43110411f035aee70e0fc3535e
Rafael Boszko <rafael.boszko@gmail.com>
2022-05-03 01:25:26 +0200

    0011 feat: setup gime command (prompt user when trying to merge)

--- gitbro/gime/BashGitMergeBranch.py
+++ gitbro/gime/BashGitMergeBranch.py
@@ -8,9 +8,13 @@ class BashGitMergeBranch:
     action: str = 'merge'
     flags: list = []
     target: str = ''
+    question: str = 'Are you sure you want to merge the branch ({branch}) into this one? (Yy|Nn)'
 
     def __init__(self, options: list = [], values: list = []) -> None:
         command = self.__map_command(options, values)
+
+        if not self.__confirm_just_in_case():
+            return
 
         # TODO: colorful print - print('{0} {1} {2}'.format('\033[32mgit', self.action, 'option'))
         print(command)
@@ -26,6 +30,13 @@ class BashGitMergeBranch:
         self.line = self.line.format(base=self.base, action=self.action, flags=' '.join(self.flags), target=self.target)
 
         return self.line
+
+    def __confirm_just_in_case(self):
+        answer = input(self.question.format(branch=self.target))
+        if answer == 'y' or answer == 'Y':
+            return True
+
+        return False
 
     def __map_command_options(self, options, values):
         if '-l' == options[0] and len(values) == 0:
