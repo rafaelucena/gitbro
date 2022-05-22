aae6fb6d40c6fad5685e5d2d6a42df98a424ab9b
Rafael Boszko <rafael.boszko@gmail.com>
2022-05-22 13:41:39 +0200

    0015 add: add argparser setup with gist (gist command mostly done with arguments and positional arguments mapped)

--- gitbro/gist/BashGitStatus.py
+++ gitbro/gist/BashGitStatus.py
@@ -1,4 +1,5 @@
 import os
+import re as regex
 from typing import Any
 from gitbro.abc.ArgumentsParser import ArgumentsParser
 from gitbro.abc.ListResultsCaseIgnored import ListResultsCaseIgnored
@@ -12,6 +13,7 @@ class BashGitStatus:
 
     parser: Any
 
+    # TODO: implement exclusive group on arguments parsing
     options: list = [
         {'abbrev': '-s', 'name': '-short', 'argument': False, 'key_parameters': {'help': 'short git status, the default here'}},
         {'abbrev': '-l', 'name': '-long', 'argument': False, 'key_parameters': {'help': 'long git status, the default of the original command'}},
@@ -19,6 +21,7 @@ class BashGitStatus:
         {'abbrev': '-a', 'name': '-all-untracked', 'argument': False, 'key_parameters': {'help': 'see all untracked files'}},
         {'abbrev': '-u', 'name': '-untracked', 'argument': False, 'key_parameters': {'help': 'see untracked files in the normal mode'}},
         {'abbrev': '-t', 'name': '-tracked', 'argument': False, 'key_parameters': {'help': 'see tracked files only'}},
+        {'abbrev': '', 'name': 'partial_name', 'argument': None, 'key_parameters': {'help': 'partial name of the file or files to show'}},
     ]
 
     def __init__(self) -> None:
@@ -31,6 +34,7 @@ class BashGitStatus:
 
     def __map_command(self, options: list) -> str:
         self.__map_command_options(options)
+        self.__map_command_value(options)
 
         self.line = self.line.format(base=self.base, action=self.action, flags=' '.join(self.flags), target=self.target)
 
@@ -56,6 +60,28 @@ class BashGitStatus:
 
         if options.b:
             self.flags.append('--branch')
+
+    def __map_command_value(self, options):
+        if options.partial_name:
+            self.target = self.__prepare_common_value(options.partial_name)
+
+    def __prepare_common_value(self, original_value):
+        filesList = ListResultsCaseIgnored()
+        updated_value = filesList.find_changed_files_for_diff(original_value)
+
+        if original_value == updated_value:
+            updated_value = filesList.find_untracked_files_for_add(original_value)
+
+        return self.__prepare_value_wildcards(updated_value)
+
+    def __prepare_value_wildcards(self, value):
+        target_value = ''
+        if regex.search('\.\w?', value):
+            target_value = '*{file_name}'.format(file_name=value)
+        else:
+            target_value = '*{file_name}*'.format(file_name=value)
+
+        return target_value
 
     @staticmethod
     def go():
