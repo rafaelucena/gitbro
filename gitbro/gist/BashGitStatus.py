17bbccc2d79f327492edd081d823b35eecd000c9
Rafael Boszko <rafael.boszko@gmail.com>
2022-05-22 13:00:30 +0200

    0015 add: add argparser setup with gist (adding variants for untracked files)

--- gitbro/gist/BashGitStatus.py
+++ gitbro/gist/BashGitStatus.py
@@ -16,7 +16,9 @@ class BashGitStatus:
         {'abbrev': '-s', 'name': '-short', 'argument': False, 'key_parameters': {'help': 'short git status, the default here'}},
         {'abbrev': '-l', 'name': '-long', 'argument': False, 'key_parameters': {'help': 'long git status, the default of the original command'}},
         {'abbrev': '-b', 'name': '-branch', 'argument': False, 'key_parameters': {'help': 'show the branch even on short mode'}},
-        # {'abbrev': '-g', 'name': '-grep', 'argument': True, 'key_parameters': {'help': 'grep', 'metavar': 'partial_name', 'dest': 'partial'}},
+        {'abbrev': '-a', 'name': '-all-untracked', 'argument': False, 'key_parameters': {'help': 'see all untracked files'}},
+        {'abbrev': '-u', 'name': '-untracked', 'argument': False, 'key_parameters': {'help': 'see untracked files in the normal mode'}},
+        {'abbrev': '-t', 'name': '-tracked', 'argument': False, 'key_parameters': {'help': 'see tracked files only'}},
     ]
 
     def __init__(self) -> None:
@@ -38,10 +40,19 @@ class BashGitStatus:
         if self.parser.is_any_argument() == False:
             self.flags.append('--short')
             self.flags.append('--branch')
-        elif options.s:
+            return
+
+        if options.s:
             self.flags.append('--short')
         elif options.l:
             self.flags.append('--long')
+
+        if options.u:
+            self.flags.append('--untracked-files=normal')
+        elif options.t:
+            self.flags.append('--untracked-files=no')
+        elif options.a:
+            self.flags.append('--untracked-files=all')
 
         if options.b:
             self.flags.append('--branch')
